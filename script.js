31f39ec78ce2f53da53aeadc4b0dfdbc92f540df
Esraa Emary <162044733+esraa-emary@users.noreply.github.com>
2024-09-03 16:51:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,27 +11,27 @@ function toggleMode() {
     const logo = document.getElementById('name');
 
     if (isDarkMode) {
-        text.style.color = '#9e225c';
-        links.forEach(link => link.style.color = '#9e225c');
-        p.forEach(effect => effect.style.color = '#9e225c');
-        h3.forEach(effect => effect.style.color = '#9e225c');
-        h1.forEach(effect => effect.style.color = '#9e225c');
+        text.style.color = '#ee84af';
+        links.forEach(link => link.style.color = '#ee84af');
+        p.forEach(effect => effect.style.color = '#ee84af');
+        h3.forEach(effect => effect.style.color = '#ee84af');
+        h1.forEach(effect => effect.style.color = '#ee84af');
     } else {
-        text.style.color = 'rgb(75, 124, 196)';
-        links.forEach(link => link.style.color = 'rgb(75, 124, 196)');
-        p.forEach(effect => effect.style.color = 'rgb(75, 124, 196)');
-        h3.forEach(effect => effect.style.color = 'rgb(75, 124, 196)');
-        h1.forEach(effect => effect.style.color = 'rgb(75, 124, 196)');
+        text.style.color = '#95e9c6';
+        links.forEach(link => link.style.color ='#95e9c6');
+        p.forEach(effect => effect.style.color = '#95e9c6');
+        h3.forEach(effect => effect.style.color = '#95e9c6');
+        h1.forEach(effect => effect.style.color = '#95e9c6');
     }
 
     isDarkMode = !isDarkMode;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('toggleText').style.color = '#9e225c';
-    document.querySelectorAll('a').forEach(link => link.style.color = '#9e225c');
-    document.querySelectorAll('h2').forEach(effect => effect.style.color = '#9e225c');
-    document.querySelectorAll('h1').forEach(effe => effe.style.color = '#9e225c');
+    document.getElementById('toggleText').style.color = '#ee84af';
+    document.querySelectorAll('a').forEach(link => link.style.color = '#ee84af');
+    document.querySelectorAll('h2').forEach(effect => effect.style.color = '#ee84af');
+    document.querySelectorAll('h1').forEach(effe => effe.style.color = '#ee84af');
 });
 
 // ====================================== home ====================================== //
@@ -41,14 +41,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const snowflakeCount = 15;
     for (let i = 0; i < snowflakeCount; i++) {
         const snowflake = document.createElement('img');
-        snowflake.src = 'https://github.com/esraa-emary/photos/blob/main/blue.png?raw=true';
+        snowflake.src = 'https://github.com/esraa-emary/photos/blob/main/blu.png?raw=true';
         snowflake.classList.add('snowflake');
         snowflake.style.left = Math.random() * 97 + 'vw';
         snowflake.style.animationDuration = (Math.random() * 10 + 2) + 's';
         snowflake.style.opacity = Math.random();
         snowContainer.appendChild(snowflake);
         const snowflake2 = document.createElement('img');
-        snowflake2.src = 'https://github.com/esraa-emary/photos/blob/main/pink.png?raw=true';
+        snowflake2.src = 'https://github.com/esraa-emary/photos/blob/main/pin.png?raw=true';
         snowflake2.classList.add('snowflake2');
         snowflake2.style.left = Math.random() * 97 + 'vw';
         snowflake2.style.animationDuration = (Math.random() * 10 + 2) + 's';
@@ -82,4 +82,8 @@ document.getElementById("expand-front").addEventListener("click", function() {
     } else {
         myDiv.style.display = "none";
     }
+});
+
+document.querySelector('.navbar__toggle').addEventListener('click', function() {
+    document.querySelector('.navbar').classList.toggle('active');
 });

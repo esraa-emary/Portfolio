8e9b403d2dea339938a1e045652fa1d697581e55
Esraa Emary <162044733+esraa-emary@users.noreply.github.com>
2024-09-21 23:02:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -28,11 +28,38 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 }
 
+window.onscroll = () => {
+    let top = window.scrollY;
+    let windowHeight = window.innerHeight;
+    let docHeight = document.documentElement.scrollHeight;
+
+    sections.forEach(sec => {
+        let offset = sec.offsetTop - 300;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                let activeLink = document.querySelector('header nav a[href*=' + id + ']');
+                if (activeLink) {
+                    activeLink.classList.add('active');
+                }
+            });
+        }
+    });
+    
+    if (window.scrollY + windowHeight >= docHeight - 300) {
+        navLinks.forEach(link => link.classList.remove('active'));
+        document.querySelector('header nav a[href*=contact]').classList.add('active');
+    }
+};
+
 document.getElementById("expand-cer").addEventListener("click", function() {
     var myDiv = document.getElementById("cer-exp");
     if (myDiv.style.display === "" || myDiv.style.display === "none") {
-        myDiv.style.display = "block";
-         myDiv.style.flexDirection = "column";
+        myDiv.style.display = "flex";
+        myDiv.style.flexDirection = "column";
         myDiv.style.gap = "1rem";
     } else {
         myDiv.style.display = "none";
